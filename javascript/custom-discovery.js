19eeb0ea57c1da4a5c6b050582c75e5432f012c4
hhgiang3698 <104768119+hhgiang3698@users.noreply.github.com>
2022-10-11 17:05:27 +0700

    Add files via upload

--- javascript/custom-discovery.js
+++ javascript/custom-discovery.js
@@ -30,3 +30,25 @@ breakpoints: {
       }
   }
 });
+
+//DROP DOWN
+$(document).ready(function () {
+  //Show and remove Dropdown
+  $(document).on('click', '.theSORT__item', function () {
+    $('.theSORT__item').not(this).removeClass('show');
+    $(this).toggleClass('show');
+  });
+  $(document).on('click', function (e) {
+    if (!$(e.target).closest('.theSORT__item').length)
+      $('.theSORT__item').removeClass('show');
+  });
+
+  // REPLACE VALUE FOR FIELD
+  let options = $('.theSORT__item .dropdown-content span');
+  options.click(function (e) {
+    let selectedOption = e.target;
+    let selected = $(this).parent().siblings('h4');
+    selected.html(selectedOption.outerHTML);
+    selected.addClass('active');
+  });
+});
--- javascript/custom.js
+++ javascript/custom.js
@@ -81,3 +81,26 @@ function showImage(image){
   console.log("index", index);
   setImageGalleryIndex(innerGallery, index);
 }
+
+
+//DROP DOWN
+$(document).ready(function () {
+    //Show and remove Dropdown
+    $(document).on('click', '.theSORT__item', function () {
+      $('.theSORT__item').not(this).removeClass('show');
+      $(this).toggleClass('show');
+    });
+    $(document).on('click', function (e) {
+      if (!$(e.target).closest('.theSORT__item').length)
+        $('.theSORT__item').removeClass('show');
+    });
+  
+    // REPLACE VALUE FOR FIELD
+    let options = $('.theSORT__item .dropdown-content span');
+    options.click(function (e) {
+      let selectedOption = e.target;
+      let selected = $(this).parent().siblings('h4');
+      selected.html(selectedOption.outerHTML);
+      selected.addClass('active');
+    });
+  });
